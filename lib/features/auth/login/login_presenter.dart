2a1a83c106a97f23ba078c4e36e840b3e1a1a968
yasinarik <yasinarikist@hotmail.com>
2022-10-08 14:30:09 +0300

    Update LoginPresenter --> read description
    - Add LogInUseCase,
    - Update onTapLogin method,
    - Add missing injection accordingly,

--- lib/features/auth/login/login_presenter.dart
+++ lib/features/auth/login/login_presenter.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutter_demo/features/auth/domain/use_cases/log_in_use_case.dart';
 import 'package:flutter_demo/features/auth/login/login_navigator.dart';
 import 'package:flutter_demo/features/auth/login/login_presentation_model.dart';
 
@@ -6,9 +9,11 @@ class LoginPresenter extends Cubit<LoginViewModel> {
   LoginPresenter(
     LoginPresentationModel super.model,
     this.navigator,
+    this.usecaseLogin,
   );
 
   final LoginNavigator navigator;
+  final LogInUseCase usecaseLogin;
 
   // ignore: unused_element
   LoginPresentationModel get _model => state as LoginPresentationModel;
@@ -22,6 +27,10 @@ class LoginPresenter extends Cubit<LoginViewModel> {
   }
 
   Future<void> onTapLogin() async {
-    // TODO
+    final result = await usecaseLogin.execute(
+      username: _model.username,
+      password: _model.password,
+    );
+    log('result: $result');
   }
 }
